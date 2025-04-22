181ac66537bc10dc5f5ca7760137228ae8ca79e7
Anthony DeVarti <98314025+anthony-devarti@users.noreply.github.com>
2025-04-22 22:53:09 +0000

    a vite config now with the right repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tasha-calculator/',
+  base: '/tasha_calculator/',
   plugins: [react()],
 });
